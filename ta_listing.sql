0ad6fa3a18e533cdae63e63ddce04b0c3943c125
Boris-Chan3 <71551500+Boris-Chan3@users.noreply.github.com>
2020-10-31 14:52:42 +0800

    Update ta_listing.sql

--- ta_listing.sql
+++ ta_listing.sql
@@ -10,7 +10,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE DATABASE `ta_listing`;
+CREATE DATABASE IF NOT EXISTS `ta_listing`;
 USE `ta_listing`;
 
 --
@@ -23,6 +23,7 @@ USE `ta_listing`;
 -- Table structure for table `account`
 --
 
+DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
   `USER_ID` varchar(15) NOT NULL,
   `HASHED_PASSWORD` varchar(256) NOT NULL,
@@ -52,7 +53,16 @@ INSERT INTO `account` (`USER_ID`, `HASHED_PASSWORD`, `EMAIL`, `USER_TYPE`) VALUE
 ('tom.2017', '16d89caa318c2b9b028cf81366d227a4fb77c02f3ec20bc27f24c64ebfc3a7b230be6a2e6e37c4b171df115ccc6a01294dae073c2c586f4057c319b4af371b003d8d27e69f37547f90e4f68c07718a422f9af24ab00ca086a594e19a169ea51d', 'tom.2017@sis.smu.edu.sg', 'student'),
 ('xiaohua.2017', '0132459b8c839ec5acdb534f70d4969dff5bcf791770eb87c4907e8c7ee96e7db2942087f19f9f341e48a0e47cddc9ce8b4c0da7b2f2835b52a15a3438fb24f9cc30950521a385dc98c6881375ffe2f87646d02bd6d0e7e8e8be8431aac89340', 'xiaohua.2017@soe.smu.edu.sg', 'student'),
 ('xiaoli.2018', '5749ace379ba5565d6f46de5f78f2e3b3578b9846cb54668275c79e67d60a98c1ca2b7af5dc2e1b46d1e0fd728638b3ff34d60b19d227129cb12f3266270084562730453115330b1b800c20fd68cce4076d80ec8ae344f2b914bd7f0abad3c77', 'xiaoli.2018@soe.smu.edu.sg', 'student'),
-('yllee', '59df546881c02be09c1f5b495c0ce5c1eeb276534d04c8ebca287d54aa8a504e9976c44f5cdbc2d56c2d511de0f1ebc93982829e2d8495e5b357fd50307df817b6f20549c6d2a7007abbdc1f362ceb8b4da4acb510e9fe5c05fd7b5d4d7450f7', 'yllee@smu.edu.sg', 'professor');
+('yllee', '59df546881c02be09c1f5b495c0ce5c1eeb276534d04c8ebca287d54aa8a504e9976c44f5cdbc2d56c2d511de0f1ebc93982829e2d8495e5b357fd50307df817b6f20549c6d2a7007abbdc1f362ceb8b4da4acb510e9fe5c05fd7b5d4d7450f7', 'yllee@smu.edu.sg', 'professor'),
+('aplin', 'c3774c557faad6634459e3fc3edbf7c317985b5b9163222d12d9f5642954a83a9b4d7dc8e3508267883b9072f22ddd30f62928936326a5cb6e3533fa2646bdd2c76aa00ba3ea8288cc30960d50b8b004a5d7118e8749a8d6f1189c9feb3fce8b', 'aplin@smu.edu.sg', 'professor'),
+('benjaminjong', '0bbfd1009481e3d30dbbb760a11b319ab804aa0a1a0e58f923e58df6d5ace9ccad65a00f93b1d25ac3fc2c4143d901e92134bea25eb3d1c5f03af89835f4beebd9e0394e2166a0b115461d74affea9aae3ca65a68e39460862337477943eafb7', 'benjaminjong@smu.edu.sg', 'professor'),
+('bthuynh', '296cf3d0b7cedf4041e14e6336bbdf92a4d20526d57f484c47231d4c0a33574286408dbcddd212086032ca5c86e792004109ad44e42cf13661d0d20a314de10a7f414845cbffced9a3b1630c728e415bb2f99201c44149dfc6f925cd5d3830fd', 'bthuynh@smu.edu.sg', 'professor'),
+('charlescsy', 'faa009c7fb63e6ed90e26f949402d938a18d46c66796f22761ef88f811185e7a578ae2286993d538bd1ea3d7a60f948aa6eb98d67e6581152797fb8c90632204e3fc21d1b5302475f2b84119a84889b5de5746c1a0253cb230acfb762b6c8359', 'charlescsy@smu.edu.sg', 'professor'),
+('chchen', '236db4a19d97622dc3ad8c84b35c1d26f334adbc278d889366ee16945ce45a7206abea6b1d4ab88fb32a2ce01cc822e185bb07d1e4d2d0c4eb60d308185df311da79191d2d113fc56d26b23f5069ffa45313df15079abea9b7447f3ef84f4848', 'chchen@smu.edu.sg', 'professor'),
+('gyfeng', 'f51baf6474077b5ca797535169f033074c7c7ce9b3cc6dbf8d3a2625b639c10bb47abfbb20ee725a6c7199908d668a3c41847fd294e6220df42eb5f704a7bb2e4f1b57ad3a5090d3847fb13bf642d32bbffa08a8b80a18282f5fda76026b774d', 'gyfeng@smu.edu.sg', 'professor'),
+('lhaverkamp', '59df546881c02be09c1f5b495c0ce5c1eeb276534d04c8ebca287d54aa8a504e9976c44f5cdbc2d56c2d511de0f1ebc93982829e2d8495e5b357fd50307df817b6f20549c6d2a7007abbdc1f362ceb8b4da4acb510e9fe5c05fd7b5d4d7450f7', 'lhaverkamp@smu.edu.sg', 'professor'),
+('psseow', '59df546881c02be09c1f5b495c0ce5c1eeb276534d04c8ebca287d54aa8a504e9976c44f5cdbc2d56c2d511de0f1ebc93982829e2d8495e5b357fd50307df817b6f20549c6d2a7007abbdc1f362ceb8b4da4acb510e9fe5c05fd7b5d4d7450f7', 'psseow@smu.edu.sg', 'professor'),
+('tracyloh', '59df546881c02be09c1f5b495c0ce5c1eeb276534d04c8ebca287d54aa8a504e9976c44f5cdbc2d56c2d511de0f1ebc93982829e2d8495e5b357fd50307df817b6f20549c6d2a7007abbdc1f362ceb8b4da4acb510e9fe5c05fd7b5d4d7450f7', 'tracyloh@smu.edu.sg', 'professor');
 
 -- --------------------------------------------------------
 
@@ -60,6 +70,7 @@ INSERT INTO `account` (`USER_ID`, `HASHED_PASSWORD`, `EMAIL`, `USER_TYPE`) VALUE
 -- Table structure for table `applications`
 --
 
+DROP TABLE IF EXISTS `applications`;
 CREATE TABLE `applications` (
   `STUDENT_NAME` varchar(30) NOT NULL,
   `STUDENT_ID` varchar(15) NOT NULL,
@@ -90,6 +101,7 @@ INSERT INTO `applications` (`STUDENT_NAME`, `STUDENT_ID`, `APPLICATION_NO`, `STA
 -- Table structure for table `modules`
 --
 
+DROP TABLE IF EXISTS `modules`;
 CREATE TABLE `modules` (
   `MOD_ID` varchar(30) NOT NULL,
   `MOD_NAME` varchar(30) NOT NULL,
@@ -137,6 +149,7 @@ INSERT INTO `modules` (`MOD_ID`, `MOD_NAME`, `POSITIONS_AVAILABLE`, `JOB_SCOPE`,
 -- Table structure for table `payments`
 --
 
+DROP TABLE IF EXISTS `payments`;
 CREATE TABLE `payments` (
   `PAYMENT_ID` int(4) NOT NULL,
   `PAYMENT_AMT` int(3) NOT NULL,
@@ -161,6 +174,7 @@ INSERT INTO `payments` (`PAYMENT_ID`, `PAYMENT_AMT`, `PAYMENT_DETAILS`, `PAYMENT
 -- Table structure for table `professors`
 --
 
+DROP TABLE IF EXISTS `professors`;
 CREATE TABLE `professors` (
   `PROFESSORS_NAME` varchar(30) NOT NULL,
   `PROFESSOR_ID` varchar(12) NOT NULL
@@ -182,7 +196,16 @@ INSERT INTO `professors` (`PROFESSORS_NAME`, `PROFESSOR_ID`) VALUES
 ('Rajesh Krishna BALAN', 'rajesh'),
 ('Subhajit DATTA', 'subhajitd'),
 ('Swapna GOTTIPATI', 'swapnag'),
-('LEE Yeow Leong', 'yllee');
+('LEE Yeow Leong', 'yllee'),
+('An Ping LIN', 'aplin'),
+('Benjamin Ong', 'benjaminjong'),
+('HUYNH Bao Tan', 'bthuynh'),
+('Charles CHEN', 'charlescsy'),
+('Christopher CHEN', 'chchen'),
+('FENG Guiyun', 'gyfeng'),
+('Larry HAVERKAMP', 'lhaverkamp'),
+('SEOW Poh Sun', 'psseow'),
+('Tracy LOH', 'tracyloh');
 
 -- --------------------------------------------------------
 
@@ -190,6 +213,7 @@ INSERT INTO `professors` (`PROFESSORS_NAME`, `PROFESSOR_ID`) VALUES
 -- Table structure for table `students`
 --
 
+DROP TABLE IF EXISTS `students`;
 CREATE TABLE `students` (
   `STUDENT_NAME` varchar(30) NOT NULL,
   `STUDENT_ID` varchar(12) NOT NULL,
@@ -216,6 +240,7 @@ INSERT INTO `students` (`STUDENT_NAME`, `STUDENT_ID`, `RESUME`, `RATING`, `CURRE
 -- Table structure for table `student_experience`
 --
 
+DROP TABLE IF EXISTS `student_experience`;
 CREATE TABLE `student_experience` (
   `STUDENT_ID` varchar(15) NOT NULL,
   `STUDENT_NAME` varchar(30) NOT NULL,
